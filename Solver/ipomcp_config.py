Multiple bug fixes: Remove threshold for DoM(0) subject Add config.yaml Add subject and agent actions Fix IPOMCP bugs Fix q-value computation Improve DoM(0) subject module

Solver/ipomcp_config.py
@@ -1,8 +1,6 @@
 import os
 from typing import Union
-
 import confuse
-import torch
 
 
 class Config(object):
@@ -12,17 +10,13 @@ class Config(object):
         self.env = environment
         self.args = args
         self.game_params = None
-        self.cuda_is_available = torch.cuda.is_available()
-        torch.manual_seed(self.seed)
-        self.device = torch.device("cuda" if self.cuda_is_available else "cpu")
         self.q_values_dir, self.simulation_results_dir = self.create_experiment_dir()
 
     def create_experiment_dir(self):
         path_prefix = self.get_from_general("results_folder")
-        worker_agent = self.args.worker_tom  # self.get_from_env("agents")['worker']['agent_name']
-        manager_agent = self.args.manager_tom  # self.get_from_env("agents")['manager']['agent_name']
-        first_mover = self.args.first_mover
-        experiment_name = f'{manager_agent}_manager_{worker_agent}_worker_{first_mover}_begins_softmax_{self.args.softmax_temp}'
+        agent_tom = self.args.agent_tom  # self.get_from_env("agents")['worker']['agent_name']
+        subject_tom = self.args.subject_tom  # self.get_from_env("agents")['manager']['agent_name']
+        experiment_name = f'{subject_tom}_subject_{agent_tom}_agent_softmax_{self.args.softmax_temp}'
         q_values_dir = os.path.join(str(path_prefix), self.env, experiment_name, 'q_values')
         simulation_results_dir = os.path.join(str(path_prefix), self.env, experiment_name, 'simulation_results')
         os.makedirs(q_values_dir, exist_ok=True)
@@ -30,7 +24,7 @@ class Config(object):
         return q_values_dir, simulation_results_dir
 
     def get_agent_tom_level(self, role):
-        if role == "manager":
+        if role == "agent":
             return self.manager_tom_level
         else:
             return self.worker_tom_level

Solver/nodes.py
@@ -3,8 +3,8 @@ from typing import Callable, Union
 import numpy as np
 import numpy.random as npr
 
-from ipomcp_config import get_config
-from abstract_classes import *
+from IPOMCP_solver.Solver.ipomcp_config import get_config
+from IPOMCP_solver.Solver.abstract_classes import *
 
 
 class TreeNode(object):
@@ -13,10 +13,6 @@ class TreeNode(object):
         self.parent = parent
         self.children = {}
         self.config = get_config()
-        self.k_o = self.config.get_from_env_exploration("k_o")
-        self.k_a = self.config.get_from_env_exploration("k_a")
-        self.alpha_a = self.config.get_from_env_exploration("alpha_a")
-        self.alpha_o = self.config.get_from_env_exploration("alpha_o")
 
     def export_tree_to_json(self):
         pass
@@ -94,13 +90,13 @@ class HistoryNode(TreeNode):
             self.add_action_node(Action(child, False), False, self.children_qvalues[idx, 1])
 
     def init_q_value(self):
-        exploration_reward = self.exploration_policy.reward_function(self.exploration_policy.actions)
+        exploration_reward = self.exploration_policy.reward_function(self.exploration_policy.actions, self.observation.value)
         if self.parent is None:
             return exploration_reward
         lower_bound = np.minimum(self.parent.action.value, self.observation.value)
         upper_bound = np.maximum(self.parent.action.value, self.observation.value)
         exploration_reward[np.where(np.logical_or(self.exploration_policy.actions < lower_bound,
-                                                   self.exploration_policy.actions > upper_bound))] = -np.inf
+                                                  self.exploration_policy.actions > upper_bound))] = -np.inf
         return exploration_reward
 
     @property